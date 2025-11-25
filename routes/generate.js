535a3869ce700515b5d8d9681de2bb1d5965bd81
Dustdeep <130654638+Viste360@users.noreply.github.com>
2025-11-25 13:33:04 +0100

    Create generate.js

---
+++ routes/generate.js
@@ -0,0 +1,9 @@
+import express from "express";
+const router = express.Router();
+
+// Placeholder
+router.post("/", async (req, res) => {
+  res.json({ status: "Caption Worker Placeholder" });
+});
+
+export default router;
